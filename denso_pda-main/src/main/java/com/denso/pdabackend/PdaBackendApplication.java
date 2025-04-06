7350564377a12a01515e2401d792b4cebca64ccd
윤현민 <preweb1988@gmai.com>
2025-04-06 17:09:36 +0900

    2025.04.06 윤현민 멀티DB추가

--- denso_pda-main/src/main/java/com/denso/pdabackend/PdaBackendApplication.java
+++ denso_pda-main/src/main/java/com/denso/pdabackend/PdaBackendApplication.java
@@ -2,14 +2,23 @@ package com.denso.pdabackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
+
 
 @ComponentScan(nameGenerator = PdaBackendBeanNameGenerator.class)
 @SpringBootApplication
-public class PdaBackendApplication {
+//public class PdaBackendApplication {
+public class PdaBackendApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PdaBackendApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(PdaBackendApplication.class);
 	}
 
 }
