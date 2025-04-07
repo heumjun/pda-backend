3ccbcf31b83afee0cb84e7a736715f7a29b4ebc5
윤현민 <preweb1988@gmai.com>
2025-04-07 23:06:32 +0900

    2025.04.07 납품지시서 조회 TB변경, 입고등록시 dmes16처리 추가

--- denso_pda-main/src/main/java/com/denso/pdabackend/domain/cigma/mapper/CigmaInterfaceMapper.java
+++ denso_pda-main/src/main/java/com/denso/pdabackend/domain/cigma/mapper/CigmaInterfaceMapper.java
@@ -1,6 +1,7 @@
 package com.denso.pdabackend.domain.cigma.mapper;
 
 import com.denso.pdabackend.domain.output.dto.OutputSearchDto;
+import com.denso.pdabackend.domain.warehousing.dto.InputHistorySearchDto;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.denso.pdabackend.domain.material.dto.MaterialsReleaseDto;
@@ -8,7 +9,9 @@ import com.denso.pdabackend.domain.material.dto.MaterialsReleaseDto;
 @Mapper
 public interface CigmaInterfaceMapper {
 
+	void insertDmes16(InputHistorySearchDto.Info item) throws Exception;
 	void insertDmes18(MaterialsReleaseDto.Info item) throws Exception;
 	void insertDmes18Output(OutputSearchDto.Info item) throws Exception;
+
 
 }
--- denso_pda-main/src/main/java/com/denso/pdabackend/domain/warehousing/service/WarehousingService.java
+++ denso_pda-main/src/main/java/com/denso/pdabackend/domain/warehousing/service/WarehousingService.java
@@ -6,6 +6,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.denso.pdabackend.domain.cigma.mapper.CigmaInterfaceMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -25,6 +26,7 @@ import lombok.RequiredArgsConstructor;
 public class WarehousingService {
 
 	private final WarehousingMapper warehousingMapper;
+	private final CigmaInterfaceMapper cigmaInterfaceMapper;
 
 	public String completeFlag(WarehousingRequest params) throws Exception {
 		return warehousingMapper.completeFlag(params);
@@ -96,88 +98,97 @@ public class WarehousingService {
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");         
 		// 포맷 적용        
 		String formatedNow = now.format(formatter);
-		
-		insertList.forEach(item -> {
-			
-			try{
-				// seq 가져오기
-				HashMap<String, Object> hashMap = new HashMap<String, Object>();
-				hashMap.put("company", item.getCompany());
-				hashMap.put("factory", item.getFactory());
-				hashMap.put("st02Pno", item.getSt02Pno());
-				hashMap.put("cm08Code",item.getCm08Code());
+		try {
+			insertList.forEach(item -> {
 
-				Map<String, Object> seq = warehousingMapper.getSeq(hashMap);
-				String st02Seq = String.valueOf(seq.get("st02Seq"));
-				String lot = "";
+				try{
+					// seq 가져오기
+					HashMap<String, Object> hashMap = new HashMap<String, Object>();
+					hashMap.put("company", item.getCompany());
+					hashMap.put("factory", item.getFactory());
+					hashMap.put("st02Pno", item.getSt02Pno());
+					hashMap.put("cm08Code",item.getCm08Code());
 
-				// LOT 번호 생성
-				// 바뀐 LOT -> 품목구분 2자리 + 라벨상세구분 4자리 + 일자 8자리 -> 14자리
-				if(item.getSt02Lot() != null){
-					lot = item.getSt02Lot();
-				} else {
-					lot = item.getCm08Gbn().substring(0,2) + item.getCm08Dgbn().substring(0,4) + formatedNow;
-				}
+					Map<String, Object> seq = warehousingMapper.getSeq(hashMap);
+					String st02Seq = String.valueOf(seq.get("st02Seq"));
+					String lot = "";
 
-				// 재고 테이블 등록 - tb_st_01 테이블
-				StockDto.Info stockInfo = new StockDto.Info();
-				stockInfo.setCompany(item.getCompany());            // 회사
-				stockInfo.setFactory(item.getFactory());            // 공장
-				stockInfo.setGbn(item.getCm08Gbn());                // 품목구분
-				stockInfo.setIpQty(item.getSt02Ipqty());            // 수량
-				stockInfo.setLot(lot);                              // LOT
-				stockInfo.setCode(item.getCm08Code());              // 품목코드
-				stockInfo.setStock(item.getSt02Stok());             // 보관창고
-				stockInfo.setSt01District(item.getSt02Dist());      // 구역
-				stockInfo.setUnt(item.getSt02Ipunt());              // 입고구분
-
-				// 입고테이블 LOT SEQ 새로 생성
-				int newLotSeq = warehousingMapper.getNewLotSeq(item);
-
-				// LOT
-				item.setSt02Lot(lot);
-				// LOT SEQ
-				item.setSt02LotSeq(newLotSeq);
-				// Seq
-				item.setSt02Seq(Integer.parseInt(st02Seq));
-
-				// 재고테이블에 LOT SEQ
-				stockInfo.setLotSeq(String.valueOf(newLotSeq));
-				// 재고 테이블 업데이트(+)
-				warehousingMapper.insertOfLotStock(stockInfo);
-				// 입고 테이블 등록
-				
-				warehousingMapper.insertOfInputHistory(item);
-
-				// 유무검사 여부가 Y인 경우 tb_qa_05 테이블의 기간을 확인하여 기간안에 들어간 경우
-				// tb_qa_06 테이블 insert 시켜줘야한다.
-				if(item.getQa05Available().equals("Y")){
-					//                    Map<String, Object> chk = inputHistorySearchMapper.getAvailable(hashMap);
-					Map<String, Object> chk = warehousingMapper.getAvailable(hashMap);
-
-					// chk 값이 있는 경우 - tb_qa_06 테이블에 insert
-					if( !MapUtils.isEmpty(chk)) {
-						InspectionConfDto.Info inspectionConf = new InspectionConfDto.Info();
-						inspectionConf.setQa07Company(String.valueOf(chk.get("qa05Company")));
-						inspectionConf.setQa07Factory(String.valueOf(chk.get("qa05Factory")));
-						inspectionConf.setQa05No(String.valueOf(chk.get("qa05No")));
-						inspectionConf.setQa07Code(String.valueOf(chk.get("qa05Code")));
-						inspectionConf.setQa07Gbn("");
-						inspectionConf.setQa07Dat(formatedNow);
-						inspectionConf.setQa07Status(String.valueOf(item.getSt02Status()));
-						inspectionConf.setQa07No(null);
-						inspectionConf.setQa07Lot(lot);
-						inspectionConf.setQa07LotSeq(String.valueOf(newLotSeq));
-						inspectionConf.setQa07Empno(item.getSt02Empno());
-
-						//                        inputHistorySearchMapper.insertInspectionConf(inspectionConf);
-						warehousingMapper.insertInspectionConf(inspectionConf);
+					// LOT 번호 생성
+					// 바뀐 LOT -> 품목구분 2자리 + 라벨상세구분 4자리 + 일자 8자리 -> 14자리
+					if(item.getSt02Lot() != null){
+						lot = item.getSt02Lot();
+					} else {
+						lot = item.getCm08Gbn().substring(0,2) + item.getCm08Dgbn().substring(0,4) + formatedNow;
 					}
+
+					// 재고 테이블 등록 - tb_st_01 테이블
+					StockDto.Info stockInfo = new StockDto.Info();
+					stockInfo.setCompany(item.getCompany());            // 회사
+					stockInfo.setFactory(item.getFactory());            // 공장
+					stockInfo.setGbn(item.getCm08Gbn());                // 품목구분
+					stockInfo.setIpQty(item.getSt02Ipqty());            // 수량
+					stockInfo.setLot(lot);                              // LOT
+					stockInfo.setCode(item.getCm08Code());              // 품목코드
+					stockInfo.setStock(item.getSt02Stok());             // 보관창고
+					stockInfo.setSt01District(item.getSt02Dist());      // 구역
+					stockInfo.setUnt(item.getSt02Ipunt());              // 입고구분
+
+					// 입고테이블 LOT SEQ 새로 생성
+					int newLotSeq = warehousingMapper.getNewLotSeq(item);
+
+					// LOT
+					item.setSt02Lot(lot);
+					// LOT SEQ
+					item.setSt02LotSeq(newLotSeq);
+					// Seq
+					item.setSt02Seq(Integer.parseInt(st02Seq));
+
+					// 재고테이블에 LOT SEQ
+					stockInfo.setLotSeq(String.valueOf(newLotSeq));
+					// 재고 테이블 업데이트(+)
+					warehousingMapper.insertOfLotStock(stockInfo);
+					// 입고 테이블 등록
+
+					warehousingMapper.insertOfInputHistory(item);
+
+					// 유무검사 여부가 Y인 경우 tb_qa_05 테이블의 기간을 확인하여 기간안에 들어간 경우
+					// tb_qa_06 테이블 insert 시켜줘야한다.
+					if(item.getQa05Available().equals("Y")){
+						//                    Map<String, Object> chk = inputHistorySearchMapper.getAvailable(hashMap);
+						Map<String, Object> chk = warehousingMapper.getAvailable(hashMap);
+
+						// chk 값이 있는 경우 - tb_qa_06 테이블에 insert
+						if( !MapUtils.isEmpty(chk)) {
+							InspectionConfDto.Info inspectionConf = new InspectionConfDto.Info();
+							inspectionConf.setQa07Company(String.valueOf(chk.get("qa05Company")));
+							inspectionConf.setQa07Factory(String.valueOf(chk.get("qa05Factory")));
+							inspectionConf.setQa05No(String.valueOf(chk.get("qa05No")));
+							inspectionConf.setQa07Code(String.valueOf(chk.get("qa05Code")));
+							inspectionConf.setQa07Gbn("");
+							inspectionConf.setQa07Dat(formatedNow);
+							inspectionConf.setQa07Status(String.valueOf(item.getSt02Status()));
+							inspectionConf.setQa07No(null);
+							inspectionConf.setQa07Lot(lot);
+							inspectionConf.setQa07LotSeq(String.valueOf(newLotSeq));
+							inspectionConf.setQa07Empno(item.getSt02Empno());
+
+							//                        inputHistorySearchMapper.insertInspectionConf(inspectionConf);
+							warehousingMapper.insertInspectionConf(inspectionConf);
+						}
+					}
+				}catch(Exception e) {
+					e.printStackTrace();
 				}
-			}catch(Exception e) {
-				e.printStackTrace();
-			}
-		});
+			});
+			// 입고이력 등록 시그마 연계전 LOTDB
+			cigmaInterfaceMapper.insertDmes16(insertList.get(0));
+
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+
+
 
 		return true;
 	}
